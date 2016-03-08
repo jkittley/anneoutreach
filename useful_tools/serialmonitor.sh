6da0756a285e259f4f02daa7243846515d248f6f
jkittley <jkd3g11@soton.ac.uk>
2016-03-08 15:07:00 +0000

    Removed virtual environments

--- useful_tools/serialmonitor.sh
+++ useful_tools/serialmonitor.sh
@@ -3,7 +3,6 @@
 echo "Press CTRL and C to terminate. Do not close the window!"
 echo "-------------------------------------------------------"
 
-source ~/wwww/venv/bin/activate
 sudo python3 ~/www/ocean-scan/serialmonitor.py --prod
 
 echo "Press enter to close window"
--- useful_tools/webserver.sh
+++ useful_tools/webserver.sh
@@ -2,7 +2,6 @@
 echo "Press CTRL and C to terminate. Do not close the window!"
 echo "-------------------------------------------------------"
 
-source ~/wwww/venv/bin/activate
 sudo python3 ~/www/ocean-scan/webserver.py --prod
 
 echo "Press enter to close window"
