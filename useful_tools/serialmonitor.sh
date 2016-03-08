7e313ea5fc70ba1dfdce13113837cb1258c31be7
jkittley <jkd3g11@soton.ac.uk>
2016-03-08 15:03:01 +0000

    Updated names

--- useful_tools/serialmonitor.sh
+++ useful_tools/serialmonitor.sh
@@ -4,7 +4,7 @@ echo "Press CTRL and C to terminate. Do not close the window!"
 echo "-------------------------------------------------------"
 
 source ~/wwww/venv/bin/activate
-sudo python3 ~/www/anneoutreach/serialmonitor.py --prod
+sudo python3 ~/www/ocean-scan/serialmonitor.py --prod
 
 echo "Press enter to close window"
 read
--- useful_tools/webserver.sh
+++ useful_tools/webserver.sh
@@ -3,7 +3,7 @@ echo "Press CTRL and C to terminate. Do not close the window!"
 echo "-------------------------------------------------------"
 
 source ~/wwww/venv/bin/activate
-sudo python3 ~/www/anneoutreach/webserver.py --prod
+sudo python3 ~/www/ocean-scan/webserver.py --prod
 
 echo "Press enter to close window"
 read
