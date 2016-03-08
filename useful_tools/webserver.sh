739e282455a771f8b6ed9051841420426c983e78
jkittley <jkd3g11@soton.ac.uk>
2016-03-08 14:17:24 +0000

    Added helpful tools to launch services

---
+++ useful_tools/webserver.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source ~/wwww/venv/bin/activate
+sudo python3 ~/www/anneoutreach/webserver.py --prod
---
+++ useful_tools/whats_my_ip.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "The IP address off this machine is:"
+hostname -I
+echo "Press enter to exit"
+read
