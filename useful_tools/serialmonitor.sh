3c83e03b2c5deaf023490cf475cc182452802f68
jkittley <jkd3g11@soton.ac.uk>
2016-03-08 14:22:48 +0000

    Improved useful tools

--- useful_tools/serialmonitor.sh
+++ useful_tools/serialmonitor.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+echo "Press CTRL and C to terminate. Do not close the window!"
+echo "-------------------------------------------------------"
+
 source ~/wwww/venv/bin/activate
 sudo python3 ~/www/anneoutreach/serialmonitor.py --prod
+
+echo "Press enter to close window"
+read
--- useful_tools/webserver.sh
+++ useful_tools/webserver.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+echo "Press CTRL and C to terminate. Do not close the window!"
+echo "-------------------------------------------------------"
 
 source ~/wwww/venv/bin/activate
 sudo python3 ~/www/anneoutreach/webserver.py --prod
+
+echo "Press enter to close window"
+read
