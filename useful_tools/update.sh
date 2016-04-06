9931c1f09e6a1d5e100d86693e2a9bda0dfd7d8a
jkittley <jkd3g11@soton.ac.uk>
2016-04-06 12:42:11 +0100

    Modified update script

--- useful_tools/update.sh
+++ useful_tools/update.sh
@@ -3,6 +3,7 @@
 echo "Press CTRL and C to terminate. Do not close the window!"
 echo "-------------------------------------------------------"
 
+cd ~/www/ocean-scan/
 git pull
 
 echo "Done! Press enter to close window"
