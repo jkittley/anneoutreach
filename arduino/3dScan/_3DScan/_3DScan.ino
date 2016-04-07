04cc145f0a6c37969c79e1d5a1279e0ad4ba3284
jkittley <jkd3g11@soton.ac.uk>
2016-04-07 21:33:23 +0100

    Scan double

--- arduino/3dScan/_3DScan/_3DScan.ino
+++ arduino/3dScan/_3DScan/_3DScan.ino
@@ -4,38 +4,54 @@
 const int trigPinX = 2;
 const int echoPinX = 3;
 
-const int trigPinY = 4;
-const int echoPinY = 5;
+const int trigPinX2 = 4;
+const int echoPinX2 = 5;
 
-const int trigPinZ = 6;
-const int echoPinZ = 7;
+const int trigPinY = 6;
+const int echoPinY = 7;
 
-const int PULSE_TIMEOUT_X = 50000;
-const int PULSE_TIMEOUT_Y = 10000;
-const int PULSE_TIMEOUT_Z = 10000;
+const int trigPinY2 = 8;
+const int echoPinY2 = 9;
 
-int sampleInterval = 250;
-int samples_per_reading = 30;
-int baudRate = 9600;
+const int trigPinZ = 10;
+const int echoPinZ = 11;
 
-Average<float> ave(samples_per_reading);
+const int PULSE_TIMEOUT = 20000;
+const int TOLLERENCE_Y_MM = 20; 
+const int TOLLERENCE_X_MM = 20; 
+
+const int CARRAIGE_WIDTH = 80 + 20; // Extra 20 is for sensor lenses
+const int TABLE_MAX_X = 1020 - CARRAIGE_WIDTH;
+const int TABLE_MAX_Y = 610 - CARRAIGE_WIDTH;
+
+const int SAMPLE_INTERVAL = 250;
+const int SAMPLES_PER_MEASURE = 20;
+const int BAUD_RATE = 9600;
+
+Average<float> ave(SAMPLES_PER_MEASURE);
 
 void setup() {
-  // initialize serial communication:
-  Serial.begin(baudRate);
+  // initialize serial
+  Serial.begin(BAUD_RATE);
   Serial.println("ONLINE");
+  // Trigger pins
   pinMode(trigPinX, OUTPUT);
   pinMode(trigPinY, OUTPUT);
+  pinMode(trigPinX2, OUTPUT);
+  pinMode(trigPinY2, OUTPUT);
   pinMode(trigPinZ, OUTPUT);
+  // Echo pins
   pinMode(echoPinX, INPUT);
   pinMode(echoPinY, INPUT);
+  pinMode(echoPinX2, INPUT);
+  pinMode(echoPinY2, INPUT);
   pinMode(echoPinZ, INPUT);
 }
 
 void loop() {  
-  float y = messure(trigPinY, echoPinY, PULSE_TIMEOUT_Y);
-  float z = messure(trigPinZ, echoPinZ, PULSE_TIMEOUT_Z);
-  float x = messure(trigPinX, echoPinX, PULSE_TIMEOUT_X);
+  float x = messureDouble(trigPinX, echoPinX, trigPinX2, echoPinX2, TABLE_MAX_X, TOLLERENCE_X_MM);
+  float y = messureDouble(trigPinY, echoPinY, trigPinY2, echoPinY2, TABLE_MAX_Y, TOLLERENCE_Y_MM);
+  float z = messureSingle(trigPinZ, echoPinZ);
   sendJSON(x, y, z);
 }
 
@@ -44,43 +60,55 @@ void sendJSON(float x, float y, float z) {
     Serial.println(s);
 }
 
-float messure(int trigPin, int echoPin, int timeout) {
-  float smax = 0;
-  float stot = 0;
+float messureSingle(int trigPin, int echoPin) {
   ave.clear();
   // Collect samples
-  for(int i=0; i< samples_per_reading; i++) {
+  for(int i=0; i< SAMPLES_PER_MEASURE; i++) {
+    ave.push(dist(trigPin, echoPin));
+    delay(20);
+  }
+  return ave.mode();
+}
+
+
+float messureDouble(int trigPin1, int echoPin1, int trigPin2, int echoPin2, int actualLength, int tolerance) {
+  int attempt = 0;
+  float m1 = 0;
+  float m2 = 0;
+  while (true) { 
+    m1 = dist(trigPin1, echoPin1);
+    delay(20);
+    m2 = dist(trigPin2, echoPin2);
+    
+    float remainder = abs(m1 + m2 - actualLength);
+    
+    if (remainder < (tolerance / 10.0)) {
+      break;
+    }
+    
+    if (attempt >= SAMPLES_PER_MEASURE) {
+        Serial.println("--> Max attempts reached");
+        break;
+    }
+  
+  } 
+  return m1;
+}
+
+float dist(int trigPin, int echoPin) {
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
-    float dist = microsecondsToCentimeters(pulseIn(echoPin, HIGH, timeout));
-    smax = max(smax, dist);
-    stot += dist;
-    ave.push(dist);
-    // Pause before continuing
-    delay(sampleInterval / samples_per_reading / 3);
-  }
-  return ave.mode();
-  //return stot / samples_per_reading;
-  //return smax;
+    return microsecondsToCentimeters(pulseIn(echoPin, HIGH, PULSE_TIMEOUT));
 }
+
 
 float microsecondsToCentimeters(long microseconds) {
   return microseconds / 29.0 / 2.0;
 }
 
-float avgSamples(float samples[]) {
-  float total = 0;
-  //Serial.print("[");
-  for(int i=0; i< samples_per_reading; i++) {
-    //Serial.print(samples[i]);
-    //Serial.print(",");
-    total = total + samples[i];
-  }
-  //Serial.println("]");
-  return total / samples_per_reading;
-}
+
 
 
