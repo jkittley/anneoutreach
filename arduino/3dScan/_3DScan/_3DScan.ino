11a4a9e099ae5eabab1b588d5ccf731f8d5a0551
jkittley <jkd3g11@soton.ac.uk>
2016-04-07 11:39:06 +0100

    AOffset to 3D surface

--- arduino/3dScan/_3DScan/_3DScan.ino
+++ arduino/3dScan/_3DScan/_3DScan.ino
@@ -1,33 +1,42 @@
 
-const int trigPin  = 3;
-const int echoPinX = 8;
-const int echoPinY = 10;
-const int echoPinZ = 9;
+#include <Average.h>
 
-const int PULSE_TIMEOUT_X = 10000;
+const int trigPinX = 2;
+const int echoPinX = 3;
+
+const int trigPinY = 4;
+const int echoPinY = 5;
+
+const int trigPinZ = 6;
+const int echoPinZ = 7;
+
+const int PULSE_TIMEOUT_X = 50000;
 const int PULSE_TIMEOUT_Y = 10000;
 const int PULSE_TIMEOUT_Z = 10000;
 
 int sampleInterval = 250;
-int samples_per_reading = 1;
+int samples_per_reading = 30;
 int baudRate = 9600;
+
+Average<float> ave(samples_per_reading);
 
 void setup() {
   // initialize serial communication:
   Serial.begin(baudRate);
   Serial.println("ONLINE");
-  pinMode(trigPin, OUTPUT);
+  pinMode(trigPinX, OUTPUT);
+  pinMode(trigPinY, OUTPUT);
+  pinMode(trigPinZ, OUTPUT);
   pinMode(echoPinX, INPUT);
   pinMode(echoPinY, INPUT);
   pinMode(echoPinZ, INPUT);
 }
 
-void loop() { 
-  float x = messure(echoPinX, PULSE_TIMEOUT_X);
-  float y = messure(echoPinY, PULSE_TIMEOUT_Y);
-  float z = messure(echoPinZ, PULSE_TIMEOUT_Z);
+void loop() {  
+  float y = messure(trigPinY, echoPinY, PULSE_TIMEOUT_Y);
+  float z = messure(trigPinZ, echoPinZ, PULSE_TIMEOUT_Z);
+  float x = messure(trigPinX, echoPinX, PULSE_TIMEOUT_X);
   sendJSON(x, y, z);
-  delay(sampleInterval);
 }
 
 void sendJSON(float x, float y, float z) {
@@ -35,20 +44,27 @@ void sendJSON(float x, float y, float z) {
     Serial.println(s);
 }
 
-float messure(int pin, int timeout) {
-  //float samples[samples_per_reading];
+float messure(int trigPin, int echoPin, int timeout) {
+  float smax = 0;
+  float stot = 0;
+  ave.clear();
   // Collect samples
-  //for(int i=0; i< samples_per_reading; i++) {
+  for(int i=0; i< samples_per_reading; i++) {
     digitalWrite(trigPin, LOW);
     delayMicroseconds(2);
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
-    //samples[i] = microsecondsToCentimeters(pulseIn(pin, HIGH, timeout));
-    //delayMicroseconds(10);
-  //}
-  return microsecondsToCentimeters(pulseIn(pin, HIGH, timeout));
-  //return avgSamples(samples);
+    float dist = microsecondsToCentimeters(pulseIn(echoPin, HIGH, timeout));
+    smax = max(smax, dist);
+    stot += dist;
+    ave.push(dist);
+    // Pause before continuing
+    delay(sampleInterval / samples_per_reading / 3);
+  }
+  return ave.mode();
+  //return stot / samples_per_reading;
+  //return smax;
 }
 
 float microsecondsToCentimeters(long microseconds) {
