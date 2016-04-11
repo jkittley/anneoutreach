00bf6d5601e8ef6f24fb8588110970f7c0e584f7
jkittley <jkd3g11@soton.ac.uk>
2016-04-11 10:01:56 +0100

    Changed to MM

--- arduino/3dScan/_3DScan/_3DScan.ino
+++ arduino/3dScan/_3DScan/_3DScan.ino
@@ -19,25 +19,31 @@ const int echoPinZ = 11;
 
 // Constants
 const int   PULSE_TIMEOUT = 10000;    // Max time to wait for a sensor to respond
-const int   TOLLERENCE_X  = 10;       // 
-const int   TOLLERENCE_Y  = 10;       // 
-const float CARRAIGE_WIDTH = 6.0;     // Width of carraige in CM
-const int   TABLE_WIDTH = 102;        // Width of table in CM
-const int   TABLE_DEPTH = 61;         // Depth of table in CM
+const int   TOLLERENCE_X  = 150;      // If you add x and x2 how far from table width is acceptable
+const int   TOLLERENCE_Y  = 150;      // If you add y and y2 how far from table depth is acceptable
+const float CARRAIGE_WIDTH = 60.0;    // Width of carraige in MM
+const float TABLE_WIDTH = 1020.0;     // Width of table in MM
+const float TABLE_DEPTH = 610.0;      // Depth of table in MM
+const float TABLE_HEIGHT = 210.0;     // Height of table in MM (sensor to floor)
 const int   MEASURE_INTERVAL = 200;   // Period between meassurements
 const int   SAMPLES_PER_MEASURE = 5;  // Number of samples used to create a messurement
 const int   BAUD_RATE = 9600;         // Serial comms rate
 
 // TMP storage vars
-int prev_x  = 0;
+int prev_x1 = 0;
 int prev_x2 = 0;
-int prev_y  = 0;
+int prev_y1 = 0;
 int prev_y2 = 0;
 Average<float> ave(SAMPLES_PER_MEASURE);
 
 void setup() {
   // initialize serial
   Serial.begin(BAUD_RATE);
+  while (!Serial) {
+    // Wait for serial to establish
+  }
+  Serial.println("{ \"status\":\"Online\"");
+   
   // Trigger pins
   pinMode(trigPinX, OUTPUT);
   pinMode(trigPinY, OUTPUT);
@@ -71,22 +77,30 @@ void reportSettings() {
 
 
 void loop() {  
-  float x  = CARRAIGE_WIDTH/2 + messure(trigPinX, echoPinX);
-  float x2 = messure(trigPinX2, echoPinX2) - CARRAIGE_WIDTH/2;
-  float y  = CARRAIGE_WIDTH/2 + messure(trigPinY, echoPinY);  
+  float x1 = CARRAIGE_WIDTH/2 + messure(trigPinX, echoPinX);
+  float x2 = CARRAIGE_WIDTH/2 + messure(trigPinX2, echoPinX2);
+  float y1 = CARRAIGE_WIDTH/2 + messure(trigPinY, echoPinY);  
   float y2 = 0.0;
-  float z  = messure(trigPinZ, echoPinZ);
+  
+  float x  = 0;
+  float y  = y1;
+  float z  = messure(trigPinZ, echoPinZ) - TABLE_HEIGHT;
 
-  if (abs(x + x2 - TABLE_WIDTH ) < TOLLERENCE_X) {
+  bool xest = false;
+  bool yest = false;
+  
+  if (abs(x1 + x2 - TABLE_WIDTH) < TOLLERENCE_X) {
     // Reading is good
-    prev_x  = x;
+    prev_x1 = x1;
     prev_x2 = x2;
+    x = x1;
   } else {
     // Reading is bad
-    float diff_x  = abs(prev_x  - x);
+    xest = true;
+    float diff_x1 = abs(prev_x1 - x1);
     float diff_x2 = abs(prev_x2 - x2);
     // Which value is closest to the last good reading
-    if (diff_x < diff_x2) {
+    if (diff_x1 < diff_x2) {
       // Do nothing i.e. use x as the trusted value
     } else {
       // Use X2 as the trusted value
@@ -94,12 +108,26 @@ void loop() {
     }
   }
 
-  sendJSON(x, y, z, x2, y2);
+  sendJSON(x, y, z, x1, y1, x2, y2, xest, yest);
 }
 
 // Output the sensor data
-void sendJSON(float x, float y, float z, float x2, float y2) {
-    String s = "{ \"x\":" + String(x) + ", \"y\":" + String(y) + ", \"z\":"  + String(z) + ", \"x2\":"  + String(x2) + ", \"y2\":"  + String(y2) +"}";
+void sendJSON(float x, float y, float z, float x1, float y1, float x2, float y2, bool xest, bool yest) {
+    String s = "{";
+    s += "\"x\":"   + String(x)  + ",";
+    s += "\"y\":"   + String(y)  + ",";
+    s += "\"z\":"   + String(z)  + ",";
+
+    s += "\"x1\":"  + String(x1) + ",";
+    s += "\"y1\":"  + String(y1) + ",";
+    
+    s += "\"x2\":"  + String(x2) + ",";
+    s += "\"y2\":"  + String(y2) + ",";
+
+    s += "\"xest\":"  + String(xest) + ",";
+    s += "\"yest\":"  + String(yest) + ",";
+    
+    s += "}";
     Serial.println(s);
 }
 
@@ -119,11 +147,11 @@ float dist(int trigPin, int echoPin) {
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
-    return microsecondsToCentimeters(pulseIn(echoPin, HIGH, PULSE_TIMEOUT));
+    return microsecondsToMM(pulseIn(echoPin, HIGH, PULSE_TIMEOUT));
 }
 
-float microsecondsToCentimeters(long microseconds) {
-  return microseconds / 29.0 / 2.0;
+float microsecondsToMM(long microseconds) {
+  return (microseconds / 29.0 / 2.0) * 10;
 }
 
 
